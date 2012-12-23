38d09766001ecd154f618efd94e76b119b980644
TomCafferty <tcafferty@glocalfocal.com>
2012-12-23 10:05:44 -0500

    first commit

---
+++ action.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Colorbox Action Plugin
+ *
+ *  Provides the jquery colorbox plugin for lightbox effect
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @license    MIT (http://opensource.org/licenses/mit-license.php) for Colorbox
+ * @author     Tom Cafferty <tcafferty@glocalfocal.com>
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once DOKU_PLUGIN.'action.php';
+
+class action_plugin_colorbox extends DokuWiki_Action_Plugin {
+
+    /**
+     * Register its handlers with the DokuWiki's event controller
+     */
+    function register(&$controller) {
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'colorbox_hookjs');
+    }
+
+    /**
+     * Hook js script into page headers.
+     *
+     * @author Tom Cafferty <tcafferty@glocalfocal.com>
+     */
+    function colorbox_hookjs(&$event, $param) {
+        global $INFO;
+        global $ID;
+        $key = 'keywords';
+        
+        $metadata = p_get_metadata($ID, $key, false);
+        
+        // keyword colorbox used to include colorbox javascript files
+        if (strpos($metadata, 'colorbox') !== false) {
+            $event->data['link'][] = array(
+                            'rel' => 'stylesheet',
+                            'type'    => 'text/css',
+                            '_data'   => '',
+                            'href'     => DOKU_BASE ."lib/scripts/colorbox/colorbox.css");
+            $event->data['script'][] = array(
+                            'type'    => 'text/javascript',
+                            'charset' => 'utf-8',
+                            '_data'   => '',
+                            'src'     => DOKU_BASE."lib/scripts/colorbox/jquery.colorbox-min.js");
+            $event->data['script'][] = array(
+                            'type'    => 'text/javascript',
+                            'charset' => 'utf-8',
+                            '_data'   => '',
+                            'src'     => DOKU_BASE."lib/scripts/colorbox/linkColorbox.js");
+       }
+    }
+}
---
+++ syntax.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Colorbox Syntax Plugin
+ *
+ *  Provides the jquery colorbox plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Tom Cafferty <tcafferty@glocalfocal.com>
+ */
+if(!defined('DOKU_INC')) define('DOKU_INC',(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_colorbox extends DokuWiki_Syntax_Plugin {
+
+    /**
+     * What kind of syntax are we?
+     */
+    function getType(){
+        return 'substition';
+    }
+
+    function getPType(){
+        return 'block';
+    }
+
+    /**
+     * Where to sort in?
+     */
+    function getSort(){
+        return 160;
+    }
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+      $this->Lexer->addSpecialPattern('<colorbox>.*?</colorbox>',$mode,'plugin_colorbox');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        parse_str($match, $return);   
+        return $return;
+    }
+
+/**
+ *
+ * Create timeline output 
+ *
+ * @author   Tom Cafferty <tcafferty@glocalfocal.com>
+ *
+ */
+    function render($mode, &$R, $data) {
+      global $conf;
+      if($mode != 'xhtml') return false;
+
+      // Initialize settings from user input or conf file
+      if (isset($data['class'])) 
+        $class = $data['class'];
+      else
+        $class = $this->getConf('class');
+        
+      if (isset($data['link'])) 
+        $link = $data['link'];
+        
+      if (isset($data['name'])) 
+        $name = $data['name'];
+        
+      // Set the colorbox link
+	  $R->doc .='<a class="'.$class.'" href="'.$link.'">'.$name.'</a>';
+	  
+	  return true;
+    }
+}
